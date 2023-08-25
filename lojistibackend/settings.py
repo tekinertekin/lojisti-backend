99896f048733c633ecd7e8abfd2aaa24c5cd8d11
bumper-tekin-ertekin <tekin.ertekin@bumper.co>
2023-08-25 19:17:12 +0300

    allowed hosts

--- lojistibackend/settings.py
+++ lojistibackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$*#u2q01yl(q_5cborc_r7$4-inhxm$le%r^bmc*r=ttu8y+3b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
