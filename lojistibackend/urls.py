1a4c936508ba8cc15534472f3b34b0c691305be4
bumper-tekin-ertekin <tekin.ertekin@bumper.co>
2023-08-30 13:57:13 +0300

    .

--- lojistibackend/urls.py
+++ lojistibackend/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    # path('api/user/', include('users.urls', namespace='users')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
